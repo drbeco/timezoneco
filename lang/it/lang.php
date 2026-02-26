9e855f407e4eb118bf4894f3cc3eb916dd0954a4
Ruben Carlo Benante <rcb@beco.cc>
2026-02-26 16:36:57 -0300

    Italian support

---
+++ lang/it/lang.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Italian language file for timezone plugin
+ *
+ * @author Ruben Carlo Benante <rcb@beco.cc>
+ */
+
+$lang['timezone'] = 'Il tuo fuso orario';
---
+++ lang/it/settings.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Italian language file for timezone plugin
+ *
+ * @author Ruben Carlo Benante <rcb@beco.cc>
+ */
+
+$lang['timezone'] = 'Imposta il fuso orario del wiki';
