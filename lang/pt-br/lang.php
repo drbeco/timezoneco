efb330856fa26164e5ba2b98a63e46ae1845b0fe
Ruben Carlo Benante <rcb@beco.cc>
2026-02-26 16:37:15 -0300

    Portuguese support added

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Brazilian Portuguese language file for timezone plugin
+ *
+ * @author Ruben Carlo Benante <rcb@beco.cc>
+ */
+
+$lang['timezone'] = 'Seu fuso horário';
---
+++ lang/pt-br/settings.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Brazilian Portuguese language file for timezone plugin
+ *
+ * @author Ruben Carlo Benante <rcb@beco.cc>
+ */
+
+$lang['timezone'] = 'Defina o fuso horário do wiki';
